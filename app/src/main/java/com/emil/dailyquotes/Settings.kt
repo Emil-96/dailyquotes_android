e650394bced58c20e0eefe41860848563050ca64
Emil <76138147+Emil-96@users.noreply.github.com>
2023-10-05 16:08:44 +0200

    added settings page

---
+++ app/src/main/java/com/emil/dailyquotes/Settings.kt
@@ -0,0 +1,13 @@
+package com.emil.dailyquotes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsPage(modifier: Modifier = Modifier){
+    Column(modifier = modifier) {
+
+    }
+}
