5e849666d25030911ff016dbc0e2e042c3f7aa34
Emil <76138147+Emil-96@users.noreply.github.com>
2023-10-10 18:09:51 +0200

    updated ui

--- app/src/main/java/com/emil/dailyquotes/Account.kt
+++ app/src/main/java/com/emil/dailyquotes/Account.kt
@@ -3,28 +3,41 @@ package com.emil.dailyquotes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountPage(
     modifier: Modifier = Modifier
 ){
-    Box(
-        modifier = modifier.fillMaxSize()
-    ) {
-        Button(
+    Scaffold(
+        modifier = modifier,
+        topBar = { TopNavBar(title = "Account") }
+    ) { paddingValues ->
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth(),
-            onClick = {
-                firebaseManager?.logOut(onSuccess = { mainActivity?.back()})
-            }
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
-            Text(text = "Log out")
+            Button(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp, vertical = 12.dp),
+                onClick = {
+                    firebaseManager?.logOut(onSuccess = { mainActivity?.back()})
+                }
+            ) {
+                Text(text = "Log out")
+            }
         }
     }
 }
