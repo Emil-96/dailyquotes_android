b0fb088ae5a2695da67c79c07e5e534fa1737a4f
Emil <76138147+Emil-96@users.noreply.github.com>
2023-10-10 11:13:39 +0200

    added firebase integration and reformatted code

--- app/src/main/java/com/emil/dailyquotes/Login.kt
+++ app/src/main/java/com/emil/dailyquotes/Login.kt
@@ -1,24 +1,23 @@
 package com.emil.dailyquotes
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedIconToggleButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,9 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
@@ -80,43 +78,78 @@ fun LoginPage(
 
             ElevatedCard(
                 modifier = Modifier
-                    .animateContentSize()
                     .fillMaxWidth()
                     .padding(horizontal = 64.dp),
                 elevation = CardDefaults.elevatedCardElevation()
             ){
 
+                var name by remember { mutableStateOf("") }
                 var email by remember { mutableStateOf("") }
                 var password by remember { mutableStateOf("") }
 
-                if(login){
-                    //password = ""
-                    LoginFields(
-                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                        email = email,
-                        setEmail = { email = it },
-                        password = password,
-                        setPassword = { password = it }
-                    )
-                }else{
-                    //password = ""
-                    SignUpFields(
-                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                        email = email,
-                        setEmail = { email = it },
-                        password = password,
-                        setPassword = { password = it }
-                    )
+                Box(
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(vertical = 16.dp, horizontal = 24.dp),
+                ) {
+                    if (login) {
+                        LoginFields(
+                            email = email,
+                            setEmail = { email = it },
+                            password = password,
+                            setPassword = { password = it }
+                        )
+                    } else {
+                        SignUpFields(
+                            name = name,
+                            setName = { name = it },
+                            email = email,
+                            setEmail = { email = it },
+                            password = password,
+                            setPassword = { password = it }
+                        )
+                    }
                 }
 
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(end = 24.dp, bottom = 16.dp),
+                        .padding(end = 24.dp, bottom = 16.dp)
+                        .animateContentSize(),
                     horizontalArrangement = Arrangement.End
                 ) {
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = if (login) "Log in" else "Sign up")
+
+                    var clicked by remember { mutableStateOf(false) }
+
+                    Button(
+                        enabled = email.isNotEmpty() && password.isNotEmpty(),
+                        onClick = {
+                            clicked = true
+                            if(login){
+                                firebaseManager?.logIn(
+                                    email = email,
+                                    password = password,
+                                    onSuccess = { mainActivity?.back() }
+                                )
+                            }else{
+                                firebaseManager?.register(
+                                    name = name,
+                                    email = email,
+                                    password = password,
+                                    onSuccess = { mainActivity?.back() }
+                                )
+                            }
+                        }
+                    ) {
+                        if(!clicked) {
+                            Text(text = if (login) "Log in" else "Sign up")
+                        }else{
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(24.dp),
+                                color = MaterialTheme.colorScheme.onPrimary,
+                                strokeWidth = 4.dp
+                            )
+                        }
                     }
                 }
             }
@@ -134,48 +167,76 @@ fun LoginFields(
     setPassword: (String) -> Unit = {}
 ){
     Column(modifier = modifier) {
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = email,
-            onValueChange = { setEmail(it) },
-            label = { Text(text = "E-Mail") }
-        )
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = password,
-            onValueChange = { setPassword(it) },
-            label = { Text(text = "Password") },
-            visualTransformation = PasswordVisualTransformation()
-        )
+        EmailField(email = email, setEmail = setEmail)
+        PasswordField(password = password, setPassword = setPassword)
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpFields(
     modifier: Modifier = Modifier,
+    name: String = "",
+    setName: (String) -> Unit = {},
     email: String = "",
     setEmail: (String) -> Unit = {},
     password: String = "",
     setPassword: (String) -> Unit = {}
 ){
     Column(modifier = modifier) {
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = email,
-            onValueChange = { setEmail(it) },
-            label = { Text(text = "E-Mail") }
-        )
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = password,
-            onValueChange = { setPassword(it) },
-            label = { Text(text = "Password") },
-            visualTransformation = PasswordVisualTransformation()
-        )
+        NameField(name = name, setName = setName)
+        EmailField(email = email, setEmail = setEmail)
+        PasswordField(password = password, setPassword = setPassword)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NameField(
+    name: String,
+    setName: (String) -> Unit
+){
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = name,
+        onValueChange = { setName(it) },
+        label = { Text(text = "Display name") },
+        keyboardOptions = KeyboardOptions(
+            capitalization = KeyboardCapitalization.Words
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailField(
+    email: String,
+    setEmail: (String) -> Unit
+){
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = email,
+        onValueChange = { setEmail(it) },
+        label = { Text(text = "E-Mail") },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordField(
+    password: String,
+    setPassword: (String) -> Unit
+){
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = password,
+        onValueChange = { setPassword(it) },
+        label = { Text(text = "Password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password
+        )
+    )
 }
