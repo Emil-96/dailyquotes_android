c1a6a65b37f12c4ad61660b64c87def81cb9db2a
emil-desktop <emil.ca.carls+git@gmail.com>
2023-11-06 22:54:38 +0100

    added local database version check

--- app/src/main/java/com/emil/dailyquotes/PreferenceManager.kt
+++ app/src/main/java/com/emil/dailyquotes/PreferenceManager.kt
@@ -2,23 +2,58 @@ package com.emil.dailyquotes
 
 import android.util.Log
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
+import com.emil.dailyquotes.room.Quote
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.util.Calendar
 
 const val PREFERENCE_KEY_QUOTE = "daily_quote"
 const val PREFERENCE_KEY_DATE = "daily_date"
+const val PREFERENCE_KEY_VERSION = "database_version"
 
-class PreferenceManager(){
+class PreferenceManager{
 
     private var _quote: MutableLiveData<Quote> = MutableLiveData()
     val quote: LiveData<Quote> = _quote
+
+    private var quoteDatabaseVersion: Int = 0
+
+    init {
+        loadInfo {
+            firebaseManager?.loadInfo()
+        }
+    }
+
+    private fun loadInfo(onFinished: () -> Unit){
+        mainActivity?.lifecycleScope?.launch {
+            mainActivity?.dataStore?.data?.collect{ preferences ->
+                quoteDatabaseVersion = preferences[intPreferencesKey(PREFERENCE_KEY_VERSION)] ?: 0
+                onFinished()
+            }
+        }
+    }
+
+    fun saveInfo(
+        databaseVersion: Int
+    ){
+        mainActivity?.lifecycleScope?.launch {
+            mainActivity?.dataStore?.edit { mutablePreferences ->
+                mutablePreferences[intPreferencesKey(PREFERENCE_KEY_VERSION)] = databaseVersion
+            }
+        }
+    }
+
+    fun getLocalDatabaseVersion(): Int{
+        return quoteDatabaseVersion
+    }
     
     fun loadDailyQuote(){
+        Log.d("PreferenceManager", "loading random quote")
         mainActivity?.lifecycleScope?.launch {
             mainActivity?.dataStore?.data?.collect { preferences ->
                 val savedDate = preferences[stringPreferencesKey(PREFERENCE_KEY_DATE)]
