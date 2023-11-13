a7dcb2e594abdde79d73e21e6631908018bb248d
emil-desktop <emil.ca.carls+git@gmail.com>
2023-11-13 06:08:02 +0100

    added placeholder dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
 
+    implementation("com.google.accompanist:accompanist-placeholder-material:0.32.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
