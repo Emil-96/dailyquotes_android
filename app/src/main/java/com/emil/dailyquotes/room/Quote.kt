c1c32a68d33b6a9c2927198737952a276fe22b7d
Emil <76138147+Emil-96@users.noreply.github.com>
2023-11-14 10:17:43 +0100

    added documentation

--- app/src/main/java/com/emil/dailyquotes/room/Quote.kt
+++ app/src/main/java/com/emil/dailyquotes/room/Quote.kt
@@ -11,6 +11,18 @@ import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.RoomDatabase
 
+/**
+ * This file contains all code required for the local database.
+ */
+
+/**
+ * Represents a quote.
+ *
+ * @param id The unique id that is also used in the remote backend.
+ * @param category The category the quote falls into.
+ * @param quote The actual text of the quote.
+ * @param quoteUrl The link to the website the quote is from.
+ */
 @Entity
 data class Quote(
     @PrimaryKey val id: String,
@@ -20,22 +32,51 @@ data class Quote(
     @ColumnInfo(name = "quote_url") val quoteUrl: String,
 )
 
+/**
+ * The Data Access Object used to interact with the local database.
+ */
 @Dao
 interface QuoteDao{
+
+    /**
+     * Returns a [List] of all [Quote] elements.
+     */
     @Query("SELECT * FROM quote")
     suspend fun getAll(): List<Quote>
 
+    /**
+     * Returns a [Quote] element corresponding to the [quoteId].
+     *
+     * @param quoteId The id of the desired [Quote] element.
+     */
     @Query("SELECT * FROM quote WHERE id = (:quoteId)")
     suspend fun getQuoteById(quoteId: String): Quote
 
+    /**
+     * Inserts all [Quote] elements in the local database.
+     *
+     * @param quotes All [Quote] elements that should get inserted into the local database.
+     */
     @Insert (onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(vararg quotes: Quote)
 
+    /**
+     * Deletion of the [Quote] element from the local database.
+     *
+     * @param quote The [Quote] element that should be deleted.
+     */
     @Delete
     suspend fun delete(quote: Quote)
 }
 
+/**
+ * The [RoomDatabase] representing the local database.
+ */
 @Database(entities = [Quote::class], version = 1)
 abstract class QuoteDatabase : RoomDatabase(){
+
+    /**
+     * Returns the Data Access Object used to interact with the database.
+     */
     abstract fun quoteDao(): QuoteDao
 }
