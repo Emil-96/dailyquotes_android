4e58ab426a0a6b95885dd2a376cf642598b8a40e
emil-siemens <emil.ca.carls@gmail.com>
2023-12-12 04:00:53 +0100

    added UI for currently signed in user (mockup)

--- app/src/main/java/com/emil/dailyquotes/Profile.kt
+++ app/src/main/java/com/emil/dailyquotes/Profile.kt
@@ -1,19 +1,31 @@
 package com.emil.dailyquotes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -46,8 +58,63 @@ fun ProfilePage(modifier: Modifier = Modifier){
                 }
             }
         }
-        NotSignedIn()
+        //NotSignedIn()
+        ProfileView()
     }
+}
+
+@Composable
+private fun ProfileView(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = modifier.padding(top = 56.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape),
+                painter = painterResource(id = R.drawable.stock_profile),
+                contentDescription = "profile image"
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 72.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                LabelText(label = "Name")
+                FieldText(text = "Joshua")
+                LabelText(label = "E-Mail")
+                FieldText(text = "joshua.felder@email.com")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            OutlinedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Edit profile")
+            }
+        }
+    }
+}
+
+@Composable
+private fun LabelText(label: String){
+    Text(
+        text = label,
+        style = MaterialTheme.typography.labelMedium,
+        modifier = Modifier.alpha(.5f)
+    )
+}
+
+@Composable
+private fun FieldText(text: String){
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+    )
 }
 
 /**
