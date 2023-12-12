652ae8b02286137919c3fa2c7b631440824b8370
emil-siemens <emil.ca.carls@gmail.com>
2023-12-12 05:14:32 +0100

    implemented profile screen

--- app/src/main/java/com/emil/dailyquotes/Profile.kt
+++ app/src/main/java/com/emil/dailyquotes/Profile.kt
@@ -18,12 +18,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
