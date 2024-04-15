cd892f1b3f8d8ccaaf95e49a856995aff4d8625a
emil-laptop <emil.ca.carls+git@gmail.com>
2024-04-15 17:46:11 +0200

    packaged screen content and navigation icons together

--- app/src/main/java/com/emil/dailyquotes/Navigation.kt
+++ app/src/main/java/com/emil/dailyquotes/Navigation.kt
@@ -13,13 +13,48 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+
+fun getNavigationDestinations(firebaseManager: FirebaseManager): List<NavigationDestination>{
+    return listOf(
+        NavigationDestination(
+            navItem = BottomNavigationItem.HomeScreenItem,
+            content = { HomeScreen() }
+        ),
+        NavigationDestination(
+            navItem = BottomNavigationItem.ProfileScreenItem,
+            content = { ProfilePage(firebaseManager = firebaseManager) }
+        )
+    )
+}
+
+class NavigationDestination(
+    private val navItem: BottomNavigationItem,
+    private val content: @Composable () -> Unit
+){
+
+    fun getNavigationItem(): BottomNavigationItem{
+        return navItem
+    }
+
+    fun getContent(modifier: Modifier = Modifier): @Composable () -> Unit{
+        return {
+            Box(
+                modifier = modifier
+            ){
+                content()
+            }
+        }
+    }
+}
 
 /**
  * Describes different items to be used in the bottom navigation bar on the home screen.
