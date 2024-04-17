c925ba3986af5ca0d4933aa7b8ffc412f51fc27d
emil-laptop <emil.ca.carls+git@gmail.com>
2024-04-17 09:47:11 +0200

    changed text boxes to single line and fixed enter behavior

--- app/src/main/java/com/emil/dailyquotes/Login.kt
+++ app/src/main/java/com/emil/dailyquotes/Login.kt
@@ -1,6 +1,5 @@
 package com.emil.dailyquotes
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
@@ -11,14 +10,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imeAnimationTarget
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
@@ -36,6 +33,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -54,14 +55,36 @@ fun LoginPage(
 ) {
 
     var login by remember { mutableStateOf(true) }
+    var clicked by remember { mutableStateOf(false) }
 
-    val imePadding = WindowInsets.imeAnimationTarget.asPaddingValues()
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
 
-    Log.d("Login", "IME Target as padding: ${imePadding.calculateBottomPadding()}")
-    Log.d(
-        "Login",
-        "IME Target and zero padding are the same: ${imePadding.calculateBottomPadding() == (0.dp)}"
-    )
+    val finishedEnabled = email.isNotEmpty() && password.isNotEmpty() && (login || password == confirmPassword)
+
+    val focusManager = LocalFocusManager.current
+
+    val finish = {
+        if(finishedEnabled) {
+            clicked = true
+            if (login) {
+                firebaseManager.logIn(
+                    email = email,
+                    password = password,
+                    onSuccess = { mainActivity?.back() }
+                )
+            } else {
+                firebaseManager.register(
+                    name = name,
+                    email = email,
+                    password = password,
+                    onSuccess = { mainActivity?.back() }
+                )
+            }
+        }
+    }
 
     Box(
         modifier = modifier
@@ -95,24 +118,37 @@ fun LoginPage(
                     .focusable(true)
             ) {
 
-                var name by remember { mutableStateOf("") }
-                var email by remember { mutableStateOf("") }
-                var password by remember { mutableStateOf("") }
-                var confirmPassword by remember { mutableStateOf("") }
-
                 Column(
                     modifier = modifier.padding(vertical = 16.dp, horizontal = 24.dp)
                 ) {
                     AnimatedVisibility(visible = !login) {
-                        NameField(name = name, setName = { name = it })
+                        NameField(
+                            name = name,
+                            setName = { name = it },
+                            focusManager = focusManager
+                        )
                     }
-                    EmailField(email = email, setEmail = { email = it })
-                    PasswordField(password = password, setPassword = { password = it })
+                    EmailField(
+                        email = email,
+                        setEmail = { email = it },
+                        focusManager = focusManager
+                    )
+                    PasswordField(
+                        isFinal = login,
+                        onDone = { finish() },
+                        password = password,
+                        setPassword = { password = it },
+                        focusManager = focusManager
+                    )
                     AnimatedVisibility(visible = !login) {
                         PasswordField(
                             isConfirm = true,
+                            isFinal = true,
+                            onDone = { finish() },
                             password = confirmPassword,
-                            setPassword = { confirmPassword = it })
+                            setPassword = { confirmPassword = it },
+                            focusManager = focusManager
+                        )
                     }
                 }
 
@@ -124,26 +160,10 @@ fun LoginPage(
                     horizontalArrangement = Arrangement.End
                 ) {
 
-                    var clicked by remember { mutableStateOf(false) }
-
                     Button(
-                        enabled = email.isNotEmpty() && password.isNotEmpty() && (login || password == confirmPassword),
+                        enabled = finishedEnabled,
                         onClick = {
-                            clicked = true
-                            if (login) {
-                                firebaseManager.logIn(
-                                    email = email,
-                                    password = password,
-                                    onSuccess = { mainActivity?.back() }
-                                )
-                            } else {
-                                firebaseManager.register(
-                                    name = name,
-                                    email = email,
-                                    password = password,
-                                    onSuccess = { mainActivity?.back() }
-                                )
-                            }
+                            finish()
                         }
                     ) {
                         if (!clicked) {
@@ -195,15 +215,23 @@ private fun TabTitle(
 @Composable
 fun NameField(
     name: String,
-    setName: (String) -> Unit
+    setName: (String) -> Unit,
+    focusManager: FocusManager
 ) {
     OutlinedTextField(
+        singleLine = true,
         modifier = Modifier.fillMaxWidth(),
         value = name,
         onValueChange = { setName(it) },
         label = { Text(text = "Display name") },
         keyboardOptions = KeyboardOptions(
-            capitalization = KeyboardCapitalization.Words
+            capitalization = KeyboardCapitalization.Words,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }
         )
     )
 }
@@ -217,15 +245,23 @@ fun NameField(
 @Composable
 fun EmailField(
     email: String,
-    setEmail: (String) -> Unit
+    setEmail: (String) -> Unit,
+    focusManager: FocusManager
 ) {
     OutlinedTextField(
+        singleLine = true,
         modifier = Modifier.fillMaxWidth(),
         value = email,
         onValueChange = { setEmail(it) },
         label = { Text(text = "E-Mail") },
         keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }
         )
     )
 }
@@ -240,17 +276,30 @@ fun EmailField(
 @Composable
 fun PasswordField(
     isConfirm: Boolean = false,
+    isFinal: Boolean,
+    onDone: () -> Unit = {},
     password: String,
-    setPassword: (String) -> Unit
+    setPassword: (String) -> Unit,
+    focusManager: FocusManager
 ) {
     OutlinedTextField(
+        singleLine = true,
         modifier = Modifier.fillMaxWidth(),
         value = password,
         onValueChange = { setPassword(it) },
         label = { Text(text = if (!isConfirm) "Password" else "Confirm password") },
         visualTransformation = PasswordVisualTransformation(),
         keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password
+            keyboardType = KeyboardType.Password,
+            imeAction = if(isFinal) ImeAction.Done else ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            },
+            onDone = {
+                onDone()
+            }
         )
     )
 }
