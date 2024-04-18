5d8448abb9d4c8b0379f7d232535c77f33daf5f4
emil-laptop <emil.ca.carls+git@gmail.com>
2024-04-18 21:58:47 +0200

    updated ui

--- app/src/main/java/com/emil/dailyquotes/Loading.kt
+++ app/src/main/java/com/emil/dailyquotes/Loading.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 
 @Composable
 fun LoadingScreen(
@@ -15,6 +16,8 @@ fun LoadingScreen(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        CircularProgressIndicator()
+        CircularProgressIndicator(
+            strokeCap = StrokeCap.Round
+        )
     }
 }
