5c35de50d252e6238a4d6ceeedc35419a8475f64
emil-laptop <emil.ca.carls+git@gmail.com>
2024-06-17 15:09:46 +0200

    updated version codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.emil.dailyquotes"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
